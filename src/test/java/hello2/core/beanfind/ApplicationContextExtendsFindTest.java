8b0e46ad28220995d6447338011d73ef8db94366
gun kim <gimgeon6053@gmail.com>
2024-01-05 18:31:40 +0900

    singletone pattern

---
+++ src/test/java/hello2/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -0,0 +1,72 @@
+package hello2.core.beanfind;
+
+import hello2.core.discount.DiscountPolicy;
+import hello2.core.discount.FixDiscountPolicy;
+import hello2.core.discount.RateDiscountPolicy;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+public class ApplicationContextExtendsFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+
+    @Test
+    @DisplayName("부모 타입으로 조회시, 자식이 둘 이상 있으면, 중복 오류가 발생한다")
+    void findBeanByParentTypeDuplicate(){
+        assertThrows(NoUniqueBeanDefinitionException.class,
+                () -> ac.getBean(DiscountPolicy.class));
+    }
+    @Test
+    @DisplayName("부모 타입으로 조회시, 자식이 둘 이상 있으면, 빈 이름을 지정하면 된다") //중복 오류 해결 방법
+    void findBeanByParentTypeBeanName(){
+        DiscountPolicy discountPolicy = ac.getBean("fixDiscountPolicy", DiscountPolicy.class);
+        assertThat(discountPolicy).isInstanceOf(RateDiscountPolicy.class);
+    }
+    @Test
+    @DisplayName("특정 하위 타입으로 조회")  // 중복 오류 해결 방법 -> 이건 안 좋은 방법임 DIP 위반
+    void findBeanBySubType(){
+        DiscountPolicy discountPolicy = ac.getBean( RateDiscountPolicy.class);
+        assertThat(discountPolicy).isInstanceOf(RateDiscountPolicy.class);
+    }
+    @Test
+    @DisplayName("부모 타입으로 모두 조회하기")
+    void findAllBeanByParentType(){
+        Map<String, DiscountPolicy> beansOfType = ac.getBeansOfType(DiscountPolicy.class);
+        assertThat(beansOfType.size()).isEqualTo(2);
+        for (String key : beansOfType.keySet()) {
+                System.out.println("key = " + key + "value = " + beansOfType.get(key));
+        }
+    }
+    @Test
+    @DisplayName("부모 타입으로 모두 조회하기 - Object")
+    void findAllBeanByObjectType(){
+        Map<String, Object> beansOfType = ac.getBeansOfType(Object.class);
+        for (String key : beansOfType.keySet()) {
+                System.out.println("key = " + key + "value = " + beansOfType.get(key));
+        }
+    }
+
+    @Configuration
+    static class TestConfig{
+
+        @Bean
+        public DiscountPolicy fixDiscountPolicy(){
+            return new FixDiscountPolicy();
+        }
+        @Bean
+        public DiscountPolicy rateDiscountPolicy(){
+            return new RateDiscountPolicy();
+        }
+
+    }
+}
---
+++ src/test/java/hello2/core/xml/XmlAppContext.java
@@ -0,0 +1,18 @@
+package hello2.core.xml;
+
+import hello2.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext(){
+        ApplicationContext ac = new GenericXmlApplicationContext();
+
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
